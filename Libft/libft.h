5d835e4cf22938b7db311944b9961227fd833ff9
MIkamal88 <109902742+MIkamal88@users.noreply.github.com>
2022-11-07 09:18:48 +0400

    libft_1

---
+++ Libft/libft.h
@@ -0,0 +1,16 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_atoi(const char *str);
+int	ft_isalnum(int i);
+int	ft_isalpha(int i);
+int ft_isascii(int i);
+int	ft_isdigit(int i);
+int	ft_isprint(int i);
+int	ft_strlen(const char *str);
+int	ft_tolower(int i);
+int	ft_toupper(int i);
+
+#endif
