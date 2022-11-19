1ff8a6a8a55a44ac74b2c488451016618ff87666
MIkamal88 <109902742+MIkamal88@users.noreply.github.com>
2022-11-19 23:47:10 +0400

    19/Nov/22_Home

---
+++ Libft/_ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mshehata <mshehata@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 16:56:34 by mshehata          #+#    #+#             */
+/*   Updated: 2022/11/19 16:56:34 by mshehata         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*list;
+
+	list = malloc(sizeof(*list));
+	if (!(list = malloc(sizeof(t_list))))
+		return (NULL);
+	list->content = content;
+	list->next = NULL;
+	return (list);
+}
